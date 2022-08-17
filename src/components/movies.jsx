5770eed28a030cf29563837f94a451e3d9b4a3c4
Rokas Vitkus <vitkus.rokas@icloud.com>
2022-08-17 10:09:47 +0200

    build Movie table

---
+++ src/components/movies.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handelDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    return <div className="mt-2">{this.renderMoviesTable()}</div>;
+  }
+
+  renderMoviesTable() {
+    const { movies } = this.state;
+    const moviesNumberInDB = movies.length;
+
+    if (moviesNumberInDB === 0)
+      return <p>There are no movies in the database.</p>;
+
+    return (
+      <>
+        <p>Showing {moviesNumberInDB} movies in the database</p>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-danger"
+                    onClick={() => this.handelDelete(movie)}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
